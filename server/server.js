0a019ddd91f4110079b0e13209eda9f3f712c4f7
Koteswararao <v.koteswararao217@gmail.com>
2026-05-30 11:52:14 +0530

    fix express route

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ app.use("/api/resume", resumeRoutes);
 
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-app.get("/*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../client/dist/index.html"));
 });
 
